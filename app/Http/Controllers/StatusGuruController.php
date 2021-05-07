247157f58cb78ec7b7d3b33c6b7ddd9c3c94ba2e
herry88 <herry081288@gmail.com>
2021-05-08 01:04:57 +0700

    AddMigration

---
+++ app/Http/Controllers/StatusGuruController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\StatusGuru;
+use Illuminate\Http\Request;
+
+class StatusGuruController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\StatusGuru  $statusGuru
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StatusGuru $statusGuru)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\StatusGuru  $statusGuru
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StatusGuru $statusGuru)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\StatusGuru  $statusGuru
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StatusGuru $statusGuru)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\StatusGuru  $statusGuru
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StatusGuru $statusGuru)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TahunAkademikController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TahunAkademik;
+use Illuminate\Http\Request;
+
+class TahunAkademikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TahunAkademik  $tahunAkademik
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TahunAkademik $tahunAkademik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TahunAkademik  $tahunAkademik
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TahunAkademik $tahunAkademik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TahunAkademik  $tahunAkademik
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TahunAkademik $tahunAkademik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TahunAkademik  $tahunAkademik
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TahunAkademik $tahunAkademik)
+    {
+        //
+    }
+}
